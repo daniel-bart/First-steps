304a4f2db81dd60abfade38b977a925e10449cd4
Daniel B <63302088+UnknOwnOn3@users.noreply.github.com>
2021-10-26 11:08:55 +0200

    Updated Version

--- ASCII-Art/Quelle.cpp
+++ ASCII-Art/Quelle.cpp
@@ -1,5 +1,9 @@
 #include <Magick++.h>
 #include <iostream> 
+#include <fstream>
+#include <filesystem>
+#include <algorithm>
+
 
 using namespace std;
 using namespace Magick;
@@ -9,8 +13,8 @@ int main(int argc, char** argv)
     InitializeMagick(*argv);
     Image image;
     try {
-        image.read("E:/Projects/ASCII-Art/pineapple.jpg");
-        Geometry newSize = Geometry(175, 115);
+        image.read("pineapple.jpg");
+        Geometry newSize = Geometry(180, 160);
         image.resize(newSize);
         cout << "Successfully loaded image!" << endl;
         int x = image.columns();
@@ -19,8 +23,10 @@ int main(int argc, char** argv)
         image.modifyImage();
         char pixel_matrix[4000][4000]; // Max. 4000*4000 Pixel
         int signs[] = {96,94,92,34,44,58,59,73,108,33,105,126,43,95,45,63,93,91,125,123,49,41,40,124,92,92,47,116,102,106,114,120,110,117,118,99,122,88,89,85,74,67,76,81,48,79,90,109,119,113,112,100,98,107,104,97,111,42,35,77,87,38,56,37,66,64,36,32};      cout << "Iterating through pixel brightness" << endl;
+        reverse(begin(signs), end(signs)); // Switching Contrast
+        ofstream art("art.txt", ios_base::app);
         for (int j = 0; j < y; j++) {
-            cout << endl;
+            art << endl;
             for (int i = 0; i < x; i++) {
 
                 Color c = image.pixelColor(i, j);
@@ -29,11 +35,11 @@ int main(int argc, char** argv)
                 int b = c.quantumBlue() * 255 / QuantumRange;
                 int brightness = ((r + g + b) / 3)/3.8;
                 pixel_matrix[i][j] = char(signs[brightness]);
-                cout << pixel_matrix[i][j]<< pixel_matrix[i][j];
+                art << pixel_matrix[i][j] << pixel_matrix[i][j] << pixel_matrix[i][j];
                 //cout <<"R: "<< r <<" G: " << g << " B: " << b << endl;
             }
         }
-
+        art.close();
     }
     catch (Exception& error_)
     {
